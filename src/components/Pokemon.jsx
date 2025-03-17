7a5872799c9841c64e9c29195b1fcf1809c273ea
teraprath <teraprath@icloud.com>
2025-03-17 02:46:57 +0100

    Element icons

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -21,16 +21,21 @@ function Pokemon({ index, data, updatePokemon }) {
                 className={`flex ${index === 0 ? "flex-col-reverse 2xl:flex-col" : "flex-col"} justify-center items-center gap-8`}
             >
                 <img
-                    className="h-60 xl:h-65 2xl:h-80"
+                    className="h-60 xl:h-65 2xl:h-80 z-10 drop-shadow-md"
                     src={data?.sprites?.other?.['official-artwork']?.front_default || "/ball.svg"}
                     alt="pokemon"
                     draggable={false}
                 />
                 <div
-                    className={`p-2 xl:p-4 bg-white text-${data ? data.types[0].type.name : "text-gray-900"}`}
+                    className={`p-2 xl:p-4 z-10 bg-white text-${data ? data.types[0].type.name : "text-gray-900"}`}
                 >
                     <p>{data ? data.name : "Loading"}</p>
                 </div>
+                <img
+                    className="absolute opacity-20 h-80 xl:h-100 2xl:h-120"
+                    src={`/elements/${data ? data.types[0].type.name : "normal"}.svg`}
+                    alt="element"
+                />
             </div>
         </button>
     )
